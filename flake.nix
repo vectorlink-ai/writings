93e6357283769b64246cd3a31f7770e1ff8d60e5
Maren van Otterdijk <maren@vectorlink.ai>
2024-10-23 09:25:31 +0200

    add bitviz to dev shell

--- flake.nix
+++ flake.nix
@@ -2,9 +2,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    bitviz.url = "github:vectorlink-ai/bitviz";
 
   };
-  outputs = {nixpkgs, flake-utils, ...}:
+  outputs = {nixpkgs, flake-utils, bitviz, ...}:
     flake-utils.lib.eachDefaultSystem (system:{
       devShells = {
         default = with nixpkgs.legacyPackages.${system};
@@ -12,6 +13,7 @@
           buildInputs = [
             nodePackages.prettier
             gnuplot
+            bitviz.packages.${system}.default
           ];
         };
       };
